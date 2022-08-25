8414fba8d1e833be351af80379b90b8e8bcc257b
Nikita Suchkov <suchkov8520@gmail.com>
2022-08-25 16:06:06 +0300

    Update js scripts

---
+++ src/js/function/burger.js
@@ -0,0 +1,12 @@
+export const menu = (() => {
+   const header = document.querySelector(".header");
+   const burger = document.querySelector(".header__toggler");
+   const nav = document.querySelector(".header__nav-collaps");
+   const overlay = document.querySelector("#overlay");
+
+   burger.addEventListener("click", () => {
+      burger.classList.toggle("header__toggler-open");
+      nav.classList.toggle("header__nav-open");
+      overlay.classList.toggle("header__overlay-open");
+   });
+})();
---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+import "./function/burger"
+import "./function/animate"
+import "./function/scroll"
